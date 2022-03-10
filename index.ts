edc0ceaf9adf6d667113ffc153095e21192a7e98
Marvin Aliaj <74553145+MarvinRoot@users.noreply.github.com>
2022-03-10 18:12:03 +0100

    added post requests for orders, items and users Added a delete request to users updated the schema.prisma with cascade on delete

--- index.ts
+++ index.ts
@@ -8,33 +8,105 @@ app.use(express.json())
 
 const prisma = new PrismaClient()
 
-app.get('/users', async (req,res) => {
-    const users = await prisma.user.findMany({include: {Order: {select: {itemId: true, quantity: true}}}})
+app.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany({ include: { Order: { select: { itemId: true, quantity: true } } } })
     res.send(users)
 })
 
-app.get('users/:id', async (req,res) => {
-    const id = req.params.id
+app.get('/users/:id', async (req, res) => {
+    const id = Number(req.params.id)
+    const user = await prisma.user.findUnique({ where: {id: id} })
+    res.send(user)
+})
+
+app.delete('/users/:id', async (req,res) => {
+    const id = Number(req.params.id)
     try{
-        const user = await prisma.user.findUnique({where: {id: id}})
-        res.send(user)
-    } catch(err) {
-        res.status(400).send(`<pre>${err.message}</pre>`)
+       const user = await prisma.user.delete({where: {id: id}})
+        res.send(user) 
+    } catch(error) {
+        // @ts-ignore
+      res.status(400).send(`<pre>${error.message}</pre>`)
     }
 })
 
-app.get('/items', async (req,res) => {
-    const items = await prisma.item.findMany({include: {Order: {select: {userId: true, quantity: true}}}})
+app.post('/users', async (req,res) => {
+    const { name, email} = req.body
+
+    try{
+        const newUser = await prisma.user.create(
+            {data: { 
+                name, email
+            }
+        })
+        res.send(newUser)
+    } catch(error) {
+        // @ts-ignore
+      res.status(400).send(`<pre>${error.message}</pre>`)
+    }
+})
+
+app.get('/orders', async (req,res) => {
+    const orders = await prisma.order.findMany()
+    res.send(orders)
+})
+
+app.get('/orders/:id', async (req,res) => {
+    const id = Number(req.params.id)
+    try{
+        const order = await prisma.order.findUnique({where: {id}})
+        res.send(order)
+    }catch(error) {
+        // @ts-ignore
+        res.status(400).send(`<pre>${error.message}</pre>`)
+    }
+})
+
+app.post('/orders', async (req,res) => {
+    const { userId, itemId, quantity} = req.body
+
+    try{
+        const newOrder = await prisma.order.create(
+            {data: { 
+                userId, itemId, quantity
+            }
+        })
+        res.send(newOrder)
+    } catch(error) {
+        // @ts-ignore
+      res.status(400).send(`<pre>${error.message}</pre>`)
+    }
+})
+
+app.get('/items', async (req, res) => {
+    const items = await prisma.item.findMany({ include: { Order: { select: { userId: true, quantity: true } } } })
     res.send(items)
 })
 
-app.get('/items/:id', async (req,res) => {
-    const id = req.params.id
+app.get('/items/:id', async (req, res) => {
+    const id = Number(req.params.id)
     try{
-        const item = prisma.item.findUnique({where: {id: id}})
+        const item = await prisma.item.findUnique({ where: {id: id} })
         res.send(item)
-    } catch(err) {
-        res.status(400).send(`<pre>${err.message}</pre>`)
+    } catch(error) {
+        // @ts-ignore
+      res.status(400).send(`<pre>${error.message}</pre>`)
+    }
+})
+
+app.post('/items', async (req,res) => {
+    const { title, image} = req.body
+
+    try{
+        const newItem = await prisma.item.create(
+            {data: { 
+                title, image
+            }
+        })
+        res.send(newItem)
+    } catch(error) {
+        // @ts-ignore
+      res.status(400).send(`<pre>${error.message}</pre>`)
     }
 })
 
